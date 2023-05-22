92718ab457ff0432502f820a8522bd220f98ba5f
Yaroslav Nykyforuk <yaroslav.nykyforuk@gmail.com>
2023-05-22 18:21:42 +0200

    chore: converted indentations to tabs.

--- components/ChatBubble.vue
+++ components/ChatBubble.vue
@@ -1,35 +1,35 @@
 <template>
-  <div
-    class="chat chat-content z-0 px-4"
-    :class="user.id === 'user' ? 'chat-end' : 'chat-start'"
-  >
-    <div class="chat-image avatar">
-      <div class="w-10 rounded-full">
-        <img :src="user.avatar"/>
-      </div>
-    </div>
-    <div class="chat-header mb-1">
-      <p class="inline mr-2">{{ user.name }}</p>
-      <time v-if="message" class="text-xs opacity-50">{{ useTimeAgo(message?.createdAt).value }}</time>
-    </div>
-    <div
-      class="chat-bubble"
-      :class="user?.id === 'user' ? 'bg-gray-600' : 'bg-gray-900'"
-    >
-      <slot>
-        <Markdown :source="message?.text" class="w-full" />
-      </slot>
-    </div>
-  </div>
+	<div
+		class="chat chat-content z-0 px-4"
+		:class="user.id === 'user' ? 'chat-end' : 'chat-start'"
+	>
+		<div class="chat-image avatar">
+			<div class="w-10 rounded-full">
+				<img :src="user.avatar"/>
+			</div>
+		</div>
+		<div class="chat-header mb-1">
+			<p class="inline mr-2">{{ user.name }}</p>
+			<time v-if="message" class="text-xs opacity-50">{{ useTimeAgo(message?.createdAt).value }}</time>
+		</div>
+		<div
+			class="chat-bubble"
+			:class="user?.id === 'user' ? 'bg-gray-600' : 'bg-gray-900'"
+		>
+			<slot>
+				<Markdown :source="message?.text" class="w-full" />
+			</slot>
+		</div>
+	</div>
 </template>
 
 <script setup lang="ts">
-  import Markdown from 'vue3-markdown-it'
-  import { Message, User } from "../types"
-  import { useTimeAgo } from '@vueuse/core'
+	import Markdown from 'vue3-markdown-it'
+	import { Message, User } from "../types"
+	import { useTimeAgo } from '@vueuse/core'
 
-  const props = defineProps<{
-    message?: Message,
-    user: User
-  }>()
+	const props = defineProps<{
+		message?: Message,
+		user: User
+	}>()
 </script>
--- server/api/ai.post.ts
+++ server/api/ai.post.ts
@@ -1,18 +1,24 @@
 import { Configuration, OpenAIApi } from "openai";
 export default defineEventHandler(async (event) => {
-  debugger
-  const body = await readBody(event);
-  const { OPENAI_API_KEY } = useRuntimeConfig();
+	debugger
+	const body = await readBody(event);
+	const { OPENAI_API_KEY } = useRuntimeConfig();
 
-  const configuration = new Configuration({
-    apiKey: OPENAI_API_KEY,
-  });
-  const openai = new OpenAIApi(configuration);
+	const configuration = new Configuration({
+		apiKey: OPENAI_API_KEY,
+	});
+	const openai = new OpenAIApi(configuration);
 
-  const completion = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: body.messages || [],
-    temperature: body.temperature || 1,
-  });
-  return completion.data;
+	const completion = await openai.createChatCompletion({
+		model: "gpt-3.5-turbo",
+		messages: [
+			{
+				role: 'system',
+				content: ''}
+			,
+			...body.messages
+		],
+		temperature: body.temperature || 1,
+	});
+	return completion.data;
 });
