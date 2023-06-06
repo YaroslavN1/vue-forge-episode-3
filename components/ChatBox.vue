dd841feca7f631bcd0da88b595ee17adcef85c1c
YaroslavNykyforuk <105391525+YaroslavN1@users.noreply.github.com>
2023-06-06 17:55:49 +0200

    feat: trained an app to be a Customer Support Agent (#3)
    * feat: added greet message in a chat box.
    
    * chore: converted indentations to tabs.
    
    * feat: added messages to go with each request to restrain chat answers.
    
    * style: chatbox window sizing.

--- components/ChatBox.vue
+++ components/ChatBox.vue
@@ -1,98 +1,107 @@
 <template>
-  <div ref="messageBox" class="chat-wrapper w-full overflow-scroll max-w-lg absolute rounded-lg bg-gray-800">
-    <button class="bg-gray-900 p-2" v-if="!showChat" @click="showChat = true" >
-      Open chat
-    </button>
-    <div v-else class="chat-opened">
-      <div @click="showChat = false" class="sticky top-0 z-10 collapse collapse-arrow bg-gray-900">
-        <div class="collapse-title">
-          Customer Support Chat
-        </div>
-      </div>
+	<div ref="messageBox" class="chat-wrapper overflow-scroll max-w-lg absolute rounded-lg bg-gray-800">
+		<button class="bg-gray-900 p-2" v-if="!showChat" @click="showChat = true" >
+			Open chat
+		</button>
+		<div v-else class="chat-opened w-full">
+			<div @click="showChat = false" class="sticky top-0 z-10 collapse collapse-arrow bg-gray-900">
+				<div class="collapse-title">
+					Customer Support Chat
+				</div>
+			</div>
+			
+			<div v-if="!messages.length" class="text-center w-[450px] m-auto pt-4">
+				<strong class="text-lg">Chat with Botman!</strong>
+				<p>Our A.I. powered assistant</p>
+				<strong class="block mt-10">Go ahead and ask us something:</strong>
+				<ul class="list-inside list-disc text-left px-6">
+					<li>What is social media post generator?</li>
+					<li>How can I get human support?</li>
+					<li>How was this tool built?</li>
+				</ul>
+			</div>
 
-      <ChatBubble
-        v-for="message in messages"
-        :key="message.id"
-        :user="getUser(message.userId)"
-        :message="message"
-      />
+			<ChatBubble
+				v-for="message in messages"
+				:key="message.id"
+				:user="getUser(message.userId)"
+				:message="message"
+			/>
 
-      <ChatBubble v-for="user in usersTyping" :key="user.id" :user="user">
-        <AppLoading/>
-      </ChatBubble>
+			<ChatBubble v-for="user in usersTyping" :key="user.id" :user="user">
+				<AppLoading/>
+			</ChatBubble>
 
-      <div class="form-control sticky bottom-0 bg-gray-800 w-full p-4 pt-8">
-          <input
-            @keydown.enter.exact="emitMessage()"
-            v-model="messageText"
-            type="text"
-            placeholder="Type here"
-            class="input input-bordered w-full bg-base-700"
-          />
-        </div>
-      </div>
-  </div>
+			<div class="form-control sticky bottom-0 bg-gray-800 w-full p-4 pt-8">
+					<input
+						@keydown.enter.exact="emitMessage()"
+						v-model="messageText"
+						type="text"
+						placeholder="Type here"
+						class="input input-bordered w-full bg-base-700"
+					/>
+				</div>
+			</div>
+	</div>
 </template>
 
 <script setup lang="ts">
-  import { ref, defineProps, defineEmits } from 'vue'
-  import type { Ref } from 'vue'
-  import { Message, User } from "../types"
-  import { nanoid } from "nanoid"
-  import ChatBubble from './ChatBubble.vue'
-  import AppLoading from './AppLoading.vue'
+	import { ref, defineProps, defineEmits } from 'vue'
+	import type { Ref } from 'vue'
+	import { Message, User } from "../types"
+	import { nanoid } from "nanoid"
+	import ChatBubble from './ChatBubble.vue'
+	import AppLoading from './AppLoading.vue'
 
-  const props = defineProps<{
-    me: User,
-    users: User[],
-    messages: Message[],
-    usersTyping?: User[]
-  }>()
+	const props = defineProps<{
+		me: User,
+		users: User[],
+		messages: Message[],
+		usersTyping?: User[]
+	}>()
 
-  const emit = defineEmits<{(e: 'new-message', payload: Message): void}>()
+	const emit = defineEmits<{(e: 'new-message', payload: Message): void}>()
 
-  const showChat: Ref<boolean> = ref(true)
+	const showChat: Ref<boolean> = ref(true)
 
-  const messageText: Ref<string> = ref('')
-  function emitMessage() {
-    emit(
-      'new-message',
-      {
-        id: nanoid(),
-        userId: "user",
-        createdAt: new Date(),
-        text: messageText.value
-      }
-    );
-    messageText.value = '';
-  }
+	const messageText: Ref<string> = ref('')
+	function emitMessage() {
+		emit(
+			'new-message',
+			{
+				id: nanoid(),
+				userId: "user",
+				createdAt: new Date(),
+				text: messageText.value
+			}
+		);
+		messageText.value = '';
+	}
 
-  const messageBox = ref<HTMLElement>()
-  watch(
-    () => props.messages.length,
-    async () => {
-      await nextTick()
-      if(messageBox.value) {
-      messageBox.value.scrollTop = messageBox.value.scrollHeight
-    }
-  })
+	const messageBox = ref<HTMLElement>()
+	watch(
+		() => props.messages.length,
+		async () => {
+			await nextTick()
+			if(messageBox.value) {
+			messageBox.value.scrollTop = messageBox.value.scrollHeight
+		}
+	})
 
-  function getUser(userId: string): User {
-    return props.users.find((user) => user.id === userId)
-  }
+	function getUser(userId: string): User {
+		return props.users.find((user) => user.id === userId)
+	}
 
 </script>
 
 <style scoped>
-
 .chat-wrapper {
-  max-height: 650px;
-  bottom: 40px;
-  right: 40px;
+	max-height: 650px;
+	bottom: 40px;
+	right: 40px;
 }
 
 .hide {
-  visibility: hidden;
+	visibility: hidden;
 }
-
 </style>
--- components/ChatWidget.vue
+++ components/ChatWidget.vue
@@ -1,25 +1,25 @@
 <template>
-  <ChatBox
-    :me="me"
-    :users="users"
-    :messages="messages"
-    @new-message="handleNewMessage"
-    :usersTyping="usersTyping"
-  />
+	<ChatBox
+		:me="me"
+		:users="users"
+		:messages="messages"
+		@new-message="handleNewMessage"
+		:usersTyping="usersTyping"
+	/>
 </template>
 
 <script setup lang="ts">
 import { Message, User } from "~~/types";
 
 const me = ref<User>({
-  id: "user",
-  avatar: "/avatar.jpg",
-  name: "You",
+	id: "user",
+	avatar: "/avatar.jpg",
+	name: "You",
 });
 const bot = ref<User>({
-  id: "assistant",
-  avatar: "/bot.jpg",
-  name: "Botman",
+	id: "assistant",
+	avatar: "/bot.jpg",
+	name: "Botman",
 });
 
 const users = computed(() => [me.value, bot.value]);
@@ -27,32 +27,32 @@ const usersTyping = ref<User[]>([]);
 
 const messages = ref<Message[]>([]);
 const messagesForAPI = computed(() =>
-  messages.value.map((m) => ({
-    role: m.userId,
-    content: m.text,
-  }))
+	messages.value.map((m) => ({
+		role: m.userId,
+		content: m.text,
+	}))
 );
 
 
 async function handleNewMessage(message: Message) {
-  messages.value.push(message);
-  usersTyping.value.push(bot.value);
-  const res = await $fetch("/api/ai", {
-    method: "POST",
-    body: {
-      messages: messagesForAPI.value,
-    },
-  });
+	messages.value.push(message);
+	usersTyping.value.push(bot.value);
+	const res = await $fetch("/api/ai", {
+		method: "POST",
+		body: {
+			messages: messagesForAPI.value,
+		},
+	});
 
-  if (!res.choices[0].message?.content) return;
+	if (!res.choices[0].message?.content) return;
 
-  const msg = {
-    id: res.id,
-    userId: bot.value.id,
-    createdAt: new Date(),
-    text: res.choices[0].message?.content,
-  };
-  messages.value.push(msg);
-  usersTyping.value = [];
+	const msg = {
+		id: res.id,
+		userId: bot.value.id,
+		createdAt: new Date(),
+		text: res.choices[0].message?.content,
+	};
+	messages.value.push(msg);
+	usersTyping.value = [];
 }
 </script>
